0026049b6dd0989f69cd5ac19a53cefcf27af599
Ömer Akkuş <118826338+akkusomer1@users.noreply.github.com>
2023-07-28 18:33:36 +0300

    Auto Mapper Configuration

---
+++ Services/Catalog/CourseMicroservices.Services.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CourseMicroservices.Services.Catalog.Dtos.Category;
+using CourseMicroservices.Services.Catalog.Dtos.Course;
+using CourseMicroservices.Services.Catalog.Dtos.Feature;
+using CourseMicroservices.Services.Catalog.Models;
+
+namespace CourseMicroservices.Services.Catalog.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Course, CreateCourseDto>().ReverseMap();
+            CreateMap<Course, UpdateCourseDto>().ReverseMap();
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+        }
+    }
+}
