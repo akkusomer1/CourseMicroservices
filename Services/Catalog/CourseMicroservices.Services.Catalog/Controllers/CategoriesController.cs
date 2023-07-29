711118d75c47016b992766992af3c5864907c5af
Ömer Akkuş <118826338+akkusomer1@users.noreply.github.com>
2023-07-29 19:14:49 +0300

    Create Category Controller

---
+++ Services/Catalog/CourseMicroservices.Services.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,32 @@
+﻿using CourseMicroservices.Services.Catalog.Dtos.Category;
+using CourseMicroservices.Services.Catalog.Dtos.Course;
+using CourseMicroservices.Services.Catalog.Interfaces;
+using CourseMicroservices.Shared.ControllerBases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseMicroservices.Services.Catalog.Controllers
+{
+    public class CategoriesController:BaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()=>
+             CreateActionResult(await _categoryService.GetAllAsync());
+        
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)=>
+             CreateActionResult(await _categoryService.GetByIdAsync(id));
+        
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCategoryDto createCategoryDto)=>     
+             CreateActionResult(await _categoryService.CreateAsync(createCategoryDto));       
+    }
+}
