4eb26869135c526712a9fb690beb482273c8c0e4
Ömer Akkuş <118826338+akkusomer1@users.noreply.github.com>
2023-07-29 19:07:43 +0300

    Create Course Controller

---
+++ Services/Catalog/CourseMicroservices.Services.Catalog/Controllers/CoursesController.cs
@@ -0,0 +1,56 @@
+﻿using CourseMicroservices.Services.Catalog.Dtos.Course;
+using CourseMicroservices.Services.Catalog.Interfaces;
+using CourseMicroservices.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseMicroservices.Services.Catalog.Controllers
+{
+    public class CoursesController : BaseController
+    {
+        private readonly ICourseService _courseService;
+
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return CreateActionResult(await _courseService.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            return CreateActionResult(await _courseService.GetByIdAsync(id));
+        }
+
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetAllByUserId(string userId)
+        {
+            return CreateActionResult(await _courseService.GetAllByUserIdAsync(userId));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCourseDto createCourseDto)
+        {
+            return CreateActionResult(await _courseService.CreateAsync(createCourseDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateCourseDto updateCourseDto)
+        {
+            return CreateActionResult(await _courseService.UpdateAsync(updateCourseDto));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            return CreateActionResult(await _courseService.DeleteAsync(id));
+        }
+
+    }
+}
---
+++ Shared/CourseMicroservices.Shared/ControllerBases/BaseController.cs
@@ -0,0 +1,32 @@
+﻿using CourseMicroservices.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseMicroservices.Shared.ControllerBases
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController:ControllerBase
+    {
+        public IActionResult CreateActionResult<T>(ResponseDto<T> response)
+        {
+            if (response.StatusCode==StatusCodes.Status204NoContent)
+            {
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode,       
+                };
+            }
+            return new ObjectResult(response.Data)
+            {
+                StatusCode = response.StatusCode,
+            };
+
+        }
+    }
+}
