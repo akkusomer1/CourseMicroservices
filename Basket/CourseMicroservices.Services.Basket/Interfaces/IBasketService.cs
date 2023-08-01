8a9d12f06d38c11e2e80b092694aa116f50d8adc
Ömer Akkuş <118826338+akkusomer1@users.noreply.github.com>
2023-08-01 09:01:47 +0300

    BasketService class

--- Basket/CourseMicroservices.Services.Basket/Interfaces/IBasketService.cs
+++ Basket/CourseMicroservices.Services.Basket/Interfaces/IBasketService.cs
@@ -5,8 +5,8 @@ namespace CourseMicroservices.Services.Basket.Interfaces
 {
     public interface IBasketService
     {
-        Task<ResponseDto<BasketDto>> GetBasket(string userld);
+        Task<ResponseDto<BasketDto>> GetBasket(string userId);
         Task<ResponseDto<bool>> SaveOrUpdate(BasketDto basketDto);
-        Task<ResponseDto<bool>> Delete(string userid);
+        Task<ResponseDto<bool>> Delete(string userId);
     }
 }
---
+++ Basket/CourseMicroservices.Services.Basket/Services/BasketService.cs
@@ -0,0 +1,47 @@
+﻿using CourseMicroservices.Services.Basket.DTOs;
+using CourseMicroservices.Services.Basket.Interfaces;
+using CourseMicroservices.Shared.Dtos;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace CourseMicroservices.Services.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase _db;
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+            _db = _redisService.GetDb();
+        }
+
+        public async Task<ResponseDto<bool>> Delete(string userId)
+        {
+            var result =await _db.KeyDeleteAsync(userId);
+            return result ? ResponseDto<bool>.Success(200) : ResponseDto<bool>.Fail("Basket Not Found", 404);
+
+        }
+
+        public async Task<ResponseDto<BasketDto>> GetBasket(string userId)
+        {
+            var basket = await _db.StringGetAsync(userId);
+
+            if (string.IsNullOrEmpty(basket))
+                return ResponseDto<BasketDto>.Fail("Basket Not Found", 404);
+
+            return ResponseDto<BasketDto>.Success(JsonSerializer.Deserialize<BasketDto>(basket!)!, 200);
+
+
+
+        }
+
+        //StringSet metodu ile hem update hemde Save işlemi yapacağız eğer ilgili key varsa update yapar yoksa yeni save yapar.
+        public async Task<ResponseDto<bool>> SaveOrUpdate(BasketDto basketDto)
+        {
+            var result = await _db.StringSetAsync(basketDto.UserId, JsonSerializer.Serialize(basketDto));
+
+            return result ? ResponseDto<bool>.Success( 200) : ResponseDto<bool>.Fail("Basket could not update or save", 500);
+        }
+    }
+}
