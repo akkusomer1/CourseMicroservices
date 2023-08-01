b37b54a8cf91f33ef10401c49bb6d6b056afe822
Ömer Akkuş <118826338+akkusomer1@users.noreply.github.com>
2023-08-01 08:10:40 +0300

    Basket Interface

---
+++ Basket/CourseMicroservices.Services.Basket/Interfaces/IBasketService.cs
@@ -0,0 +1,12 @@
+﻿using CourseMicroservices.Services.Basket.DTOs;
+using CourseMicroservices.Shared.Dtos;
+
+namespace CourseMicroservices.Services.Basket.Interfaces
+{
+    public interface IBasketService
+    {
+        Task<ResponseDto<BasketDto>> GetBasket(string userld);
+        Task<ResponseDto<bool>> SaveOrUpdate(BasketDto basketDto);
+        Task<ResponseDto<bool>> Delete(string userid);
+    }
+}
