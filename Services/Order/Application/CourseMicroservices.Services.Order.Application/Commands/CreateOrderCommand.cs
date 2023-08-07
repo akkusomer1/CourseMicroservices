9d8ff85b60abb48c2160638cb4923f69fb615ad6
Ömer Akkuş <118826338+akkusomer1@users.noreply.github.com>
2023-08-07 12:30:57 +0300

    Add Order Command

---
+++ Services/Order/Application/CourseMicroservices.Services.Order.Application/Commands/CreateOrderCommand.cs
@@ -0,0 +1,21 @@
+﻿using Azure;
+using CourseMicroservices.Services.Order.Application.DTOs;
+using CourseMicroservices.Shared.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseMicroservices.Services.Order.Application.Commands
+{
+    public class CreateOrderCommand : IRequest<ResponseDto<CreatedOrderDto>>
+    {
+        public string BuyerId { get; set; }
+
+        public List<OrderItemDto> OrderItems { get; set; }
+
+        public AddressDto Address { get; set; }
+    }
+}
---
+++ Services/Order/Application/CourseMicroservices.Services.Order.Application/Handlers/CreateOrderCommandHandle.cs
@@ -0,0 +1,32 @@
+﻿using CourseMicroservices.Services.Order.Application.Commands;
+using CourseMicroservices.Services.Order.Application.DTOs;
+using CourseMicroservices.Services.Order.Domain.OrderAggregate;
+using CourseMicroservices.Services.Order.Instrastructure;
+using CourseMicroservices.Shared.Dtos;
+using MediatR;
+using aliesOrder = CourseMicroservices.Services.Order.Domain.OrderAggregate.FreeCourse.Services.Order.Domain.OrderAggregate;
+
+namespace CourseMicroservices.Services.Order.Application.Handlers
+{
+    public class CreateOrderCommandHandle : IRequestHandler<CreateOrderCommand, ResponseDto<CreatedOrderDto>>
+    {
+        private readonly OrderDbContext _context;
+        public async Task<ResponseDto<CreatedOrderDto>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var newAddress = new Address(request.Address.Province, request.Address.District, request.Address.Street, request.Address.ZipCode, request.Address.Line);
+
+            aliesOrder.Order newOrder = new(request.BuyerId, newAddress);
+
+            request.OrderItems.ForEach(x =>
+            {
+                newOrder.AddOrderItem(x.ProductId, x.ProductName, x.Price, x.PictureUrl);
+            });
+
+            await _context.Orders.AddAsync(newOrder);
+
+            await _context.SaveChangesAsync();
+
+            return ResponseDto<CreatedOrderDto>.Success(new CreatedOrderDto { OrderId = newOrder.Id }, 200);
+        }
+    }
+}
