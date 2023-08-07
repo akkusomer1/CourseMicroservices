14963f6f7cf7101cda82a2461d7ca0fcfac01437
Ömer Akkuş <118826338+akkusomer1@users.noreply.github.com>
2023-08-07 11:43:51 +0300

    Add Cqrs Pattern and Queries process

---
+++ Services/Order/Application/CourseMicroservices.Services.Order.Application/Handlers/GetOrderByUserIdQueryHandle.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using CourseMicroservices.Services.Order.Application.DTOs;
+using CourseMicroservices.Services.Order.Application.Queries.Order;
+using CourseMicroservices.Services.Order.Instrastructure;
+using CourseMicroservices.Shared.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseMicroservices.Services.Order.Application.Handlers
+{
+    public class GetOrderByUserIdQueryHandle : IRequestHandler<GetOrderByUserIdQuery, ResponseDto<List<OrderDto>>>
+    {
+        private readonly IMapper _mapper;
+        private readonly OrderDbContext _context;
+
+        public GetOrderByUserIdQueryHandle(IMapper mapper, OrderDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task<ResponseDto<List<OrderDto>>> Handle(GetOrderByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _context.Orders.Where(x => x.BuyerId == request.UserId).ToListAsync();
+
+            if (!orders.Any())
+            {
+                return ResponseDto<List<OrderDto>>.Success(new List<OrderDto>(), 200);
+            }
+            return ResponseDto<List<OrderDto>>.Success(_mapper.Map<List<OrderDto>>(orders), 200);
+        }
+    }
+}
---
+++ Services/Order/Application/CourseMicroservices.Services.Order.Application/Queries/Order/GetOrderByUserIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using CourseMicroservices.Services.Order.Application.DTOs;
+using CourseMicroservices.Shared.Dtos;
+using MediatR;
+
+namespace CourseMicroservices.Services.Order.Application.Queries.Order
+{
+    public class GetOrderByUserIdQuery:IRequest<ResponseDto<List<OrderDto>>>
+    {
+        public string UserId { get; set; }
+    }
+}
